b0c48f11b32bca9d7861d43b2efd6dc3805e63f3
Amory Meltzer <Amorymeltzer@gmail.com>
2014-10-16 08:32:35 -0700

    Begin deltaVScience.pl to estimate science per deltaV per spob = efficiency?

---
+++ deltaVScience.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+# deltaVScience.pl by Amory Meltzer
+# Attempt to (roughly) estimate a ratio of science per deltaV for each spob
+# Takes in an averages (-a or -as) table from parseScience.pl
+
+use strict;
+use warnings;
+use diagnostics;
+
