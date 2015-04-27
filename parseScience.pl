725d415cf85beca83fbfe962d7025a0f9fdb7328
Amory Meltzer <Amorymeltzer@gmail.com>
2015-04-27 11:47:42 -0700

    Nah

--- parseScience.pl
+++ parseScience.pl
@@ -15,7 +15,6 @@
 ## Default windows/linux path to Gamedata/pers/scidefs/etc.?
 ### Steam locations
 
-## Turn cascading key/value elsifs into hash lookup?
 ## Given/while?  Requires 5.10.1 or 5.14 so maybe not ideal
 ## Cleanup data/test hashes, the order of the data is unintuitive
 
