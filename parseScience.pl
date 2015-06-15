9de84a248d321e3643a976e958d04fb63aa017f7
Amory Meltzer <Amorymeltzer@gmail.com>
2015-06-15 09:23:20 -0700

    Note the not-so-bad-anymore aspect of fake science with ScienceAlert

--- parseScience.pl
+++ parseScience.pl
@@ -592,6 +592,7 @@ foreach (0..scalar @test - 1) {
     # Skip over annoying "fake" science expts caused by ScienceAlert
     # For more info see
     # http://forum.kerbalspaceprogram.com/threads/76793-0-90-ScienceAlert-1-8-4-Experiment-availability-feedback-%28December-23%29?p=1671187&viewfull=1#post1671187
+    # Still present (especially @ KSC (see above) but not much elsewhere)
     next if !$dataMatrix{$test[$_].$spob[$_].$where[$_].$biome[$_]};
 
     $dataMatrix{$test[$_].$spob[$_].$where[$_].$biome[$_]} = [$test[$_],$spob[$_],$where[$_],$biome[$_],$dsc[$_],$scv[$_],$sbv[$_],$sci[$_],$cap[$_],$cap[$_]-$sci[$_],$percL];
